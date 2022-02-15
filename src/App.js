b0f411d93cbb0f22a0beb22ad8e922d7c02cbee7
Sait Can Tunç <tuncsaitcan@gmail.com>
2022-02-15 08:12:26 +0300

    Default content added & styles done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,35 @@ import { useState } from "react";
 import Editor from "./components/Editor";
 import Previewer from "./components/Previewer";
 
+const defaultContent = `
+![Dwinatech Logo](https://yt3.ggpht.com/ytc/AKedOLRCB8l9dwq0qVxZIb7C7b1G5lvYlFIYzymD1Dkx=s176-c-k-c0x00ffffff-no-rj)
+
+# Hello, 
+## You'r welcom at
+### DwinaTech Channel
+
+
+\`<div>Inline code</div>\`
+
+\`\`\`
+const multipleLineCode = (param) => {
+    if(param) {
+        return param
+    }
+}
+\`\`\`
+
+**Some bold text**
+
+[Visit My Channel](https://www.youtube.com/channel/UCSS0kFyF7KWjE19Rj9PgNQA)
+
+> Block Quot
+
+1. First list item
+2. Second list item
+`;
 function App() {
-	const [content, setContent] = useState("Deneme");
+	const [content, setContent] = useState(defaultContent);
 
 	const handleTextChange = (e) => {
 		setContent(e.target.value);
