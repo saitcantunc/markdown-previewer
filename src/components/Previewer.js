c03cfec7ca30f2855b94368b13c5fb7af3fae225
Sait Can Tunç <tuncsaitcan@gmail.com>
2022-02-15 07:48:14 +0300

    Added Marked and Prismjs

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,7 +1,27 @@
-import React from "react";
+import { marked } from "marked";
+import Prism from "prismjs";
+
+marked.setOptions({
+	breaks: true,
+	highlight: function (code) {
+		return Prism.highlight(code, Prism.languages.javascript, "javascript");
+	},
+});
+
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+	return `<a target="_blank" href="${href}">${text}</a>`;
+};
 
 const Previewer = ({ content }) => {
-	return <div className='preview'>{content}</div>;
+	return (
+		<div
+			className='preview'
+			dangerouslySetInnerHTML={{
+				__html: marked(content, { renderer: renderer }),
+			}}
+		/>
+	);
 };
 
 export default Previewer;
